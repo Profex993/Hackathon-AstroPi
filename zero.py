085a1cbc128c097e7203d26d5b0e795699a7e3e7
Domysli-si <s.maj683@gmail.com>
2024-12-06 20:26:14 +0100

    Defaulet state -> 1. phase finished

--- zero.py
+++ zero.py
@@ -8,33 +8,35 @@ sense.set_rotation(270, False)
 sense.color.gain = 60
 sense.color.integration_cycles = 64
 
-b = (0, 0, 0)
+a = (0, 0, 0)
+b = (0, 128, 0)
+c = (255, 255, 255)
 rgb = sense.color
-c = (rgb.red, rgb.green, rgb.blue)
+d = (rgb.red, rgb.green, rgb.blue)
 
 
 def anim():
-    sense.set_pixels([
-        b, b, b, b, b, b, b, b,
-        b, b, b, c, c, b, b, b,
-        b, c, b, c, c, b, c, b,
-        c, c, c, c, c, c, c, c,
-        b, b, c, c, c, c, b, b,
-        b, b, b, c, c, b, b, b,
-        b, c, b, b, b, b, c, b,
-        c, b, b, b, b, b, b, c
+    sense.set_pixels([ #Default state. :)
+        c, c, a, a, a, a, c, c,
+        c, c, a, a, a, a, c, c,
+        a, a, c, c, c, c, a, a,
+        c, c, a, b, b, a, c, c,
+        c, c, a, b, b, a, c, c,
+        a, a, a, b, b, a, a, a,
+        a, b, a, b, b, a, b, a,
+        a, a, b, b, b, b, a, a
     ])
     time.sleep(0.5)
 
     sense.set_pixels([
-        b, b, b, c, c, b, b, b,
-        b, b, c, c, c, c, b, b,
-        b, c, b, c, c, b, c, b,
-        b, c, c, c, c, c, c, b,
-        b, b, b, c, c, b, b, b,
-        b, b, b, c, c, b, b, b,
-        b, b, c, b, b, c, b, b,
-        b, c, b, b, b, b, c, b
+        d, d, a, a, a, a, d, d,
+        d, d, a, a, a, a, d, d,
+        a, a, d, d, d, d, a, a,
+        d, d, a, b, b, a, d, d,
+        d, d, a, b, b, a, d, d,
+        a, a, a, b, b, a, a, a,
+        a, b, a, b, b, a, b, a,
+        a, a, b, b, b, b, a, a
     ])
     time.sleep(0.5)
 
