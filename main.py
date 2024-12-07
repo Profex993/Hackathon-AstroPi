556e49853a3cabfc893e0dcf6b64ab60d8c6ff17
Profex993 <68472681+Profex993@users.noreply.github.com>
2024-12-07 10:29:07 +0100

    exif speed

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 import time
+from functools import total_ordering
 
 from exif import Image
 from datetime import datetime
 import cv2
 import math
-
 
 def get_time(image):
     with open(image, 'rb') as image_file:
@@ -68,12 +68,13 @@ def calculate_mean_distance(coordinates_1, coordinates_2):
 
 def calculate_speed_in_kmps(feature_distance, GSD, time_difference):
     distance = feature_distance * GSD / 100000
+    print(distance)
     speed = distance / time_difference
     return speed
 
-def ai_speed(imgstr, imgstr2):
-    time_difference = get_time_difference(imgstr, imgstr2)
-    image_1_cv, image_2_cv = convert_to_cv(imgstr, imgstr2)
+def get_ai_speed(image_num):
+    time_difference = get_time_difference(images[image_num], images[image_num+1])
+    image_1_cv, image_2_cv = convert_to_cv(images[image_num], images[image_num + 1])
     keypoints_1, keypoints_2, descriptors_1, descriptors_2 = calculate_features(image_1_cv, image_2_cv, 1000)
     matches = calculate_matches(descriptors_1, descriptors_2)
     coordinates_1, coordinates_2 = find_matching_coordinates(keypoints_1, keypoints_2, matches)
@@ -82,11 +83,57 @@ def ai_speed(imgstr, imgstr2):
     return speed
 #endregion ai
 
-def get_distance(imgstr): #coordinates
-    img = Image(imgstr)
-    location = img.gps_longitude
-    print(location)
+def dms_to_decimal(degrees, minutes, seconds):
+    return degrees + (minutes / 60.0) + (seconds / 3600.0)
 
+distances = []
+def get_distance(image_num):
+    total_distance = 0
+    R = 6371.0
+
+    img1 = Image(images[image_num])
+    img2 = Image(images[image_num + 1])
+
+    # Extract GPS data
+    lon1 = img1.gps_longitude
+    lat1 = img1.gps_latitude
+    lon2 = img2.gps_longitude
+    lat2 = img2.gps_latitude
+
+    # Convert DMS to decimal degrees
+    lon1 = dms_to_decimal(*lon1)
+    lat1 = dms_to_decimal(*lat1)
+    lon2 = dms_to_decimal(*lon2)
+    lat2 = dms_to_decimal(*lat2)
+
+    # Convert latitude and longitude from degrees to radians
+    lat1, lon1 = map(math.radians, [lat1, lon1])
+    lat2, lon2 = map(math.radians, [lat2, lon2])
+
+    # Differences in coordinates
+    difference_lat = lat2 - lat1
+    difference_lon = lon2 - lon1
+
+    # Haversine formula
+    a = math.sin(difference_lat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(difference_lon / 2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    # Distance
+    distance = R * c
+
+    distances.append(distance)
+
+    for distance in distances:
+        total_distance = total_distance + distance
+
+    total_distance = total_distance / len(distances)
+    print(total_distance)
+
+    return total_distance
+
+def exif_speed(image_num):
+    speed = get_distance(image_num) / get_time_difference(images[image_num], images[image_num+1])
+    return speed
 
 def init_images():
     directory = "img"
@@ -103,13 +150,17 @@ def init_images():
 
 def simulate_images():
     for i in range(len(images) - 1):
-        if (len(images[i]) != 1):
-            print(ai_speed(images[i], images[i + 1]))
-            get_distance(images[i])
+
+        if len(images[i]) != 1:
+            print("ai")
+            print(get_ai_speed(i))
+            print("exif")
+            print(exif_speed(i))
+
             print("sleep for: ")
             print(times[i] - 1)
             time.sleep(times[i - 1])
-            if (i == len(images)):
+            if i == len(images):
                 time.sleep(100)
 
 images, times = init_images()
